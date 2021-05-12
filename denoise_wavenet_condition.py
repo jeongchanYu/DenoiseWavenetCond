86d2e851b3e5657525c6f2881623c8aae0158653
정찬 유 <ab552947@gmail.com>
2021-05-12 14:01:08 +0900

    transpos conv change

--- denoise_wavenet_condition.py
+++ denoise_wavenet_condition.py
@@ -19,8 +19,11 @@ class DenoiseWavenetCondition(tf.keras.Model):
         # channel increase
         self.conv_channel = Conv1D(128, 1, padding='same')
 
+        # condition dimension increase
+        self.conv_cond_channel_up = Conv1DTranspose(1, self.input_size-self.max_condition+1)
+
         # residual block
-        self.residual_block = [ResidualBlock(d, input_size=self.input_size, max_condition=self.max_condition) for d in self.dilation]
+        self.residual_block = [ResidualBlock(d) for d in self.dilation]
 
         # skip output
         self.conv_output = [Conv1D(2048, 3, padding='same', activation='relu')]
@@ -31,6 +34,7 @@ class DenoiseWavenetCondition(tf.keras.Model):
     def call(self, x, condition):
         input = self.conv_channel(x)
 
+        condition = self.conv_cond_channel_up(condition)
         output, input = self.residual_block[0](input, condition)
         for f in self.residual_block[1:]:
             skip_output, input = f(input, condition)
@@ -53,20 +57,14 @@ class DenoiseWavenetCondition(tf.keras.Model):
 
 
 class ResidualBlock(tf.Module):
-    def __init__(self, dilation, input_size, max_condition):
-        self.input_size = input_size
-        self.max_condition = max_condition
-
+    def __init__(self, dilation):
         self.conv_gated_tanh = Conv1D(128, 3, padding='same', dilation_rate=dilation)
         self.conv_gated_sigmoid = Conv1D(128, 3, padding='same', dilation_rate=dilation)
         self.conv_skip = Conv1D(128, 1)
         self.conv_residual = Conv1D(128, 1)
 
-        tran_kernel_size = self.input_size % (self.max_condition-1)
-        while tran_kernel_size < 3:
-            tran_kernel_size += self.max_condition-1
-        self.conv_trans_condition_tanh = Conv1DTranspose(128, tran_kernel_size, strides=(self.input_size-tran_kernel_size)//(self.max_condition-1), output_padding=0)
-        self.conv_trans_condition_sigmoid = Conv1DTranspose(128, tran_kernel_size, strides=(self.input_size-tran_kernel_size)//(self.max_condition-1), output_padding=0)
+        self.conv_trans_condition_tanh = Conv1D(128, 1, padding='same')
+        self.conv_trans_condition_sigmoid = Conv1D(128, 1, padding='same')
 
     def __call__(self, input, condition):
         trans_condition_tanh = self.conv_trans_condition_tanh(condition)
